8b7229289e66a3e5be069fb06c6861808562f477
Max <imax.webdev@gmail.com>
2022-02-22 11:48:07 +0300

    Commit

--- messages.js
+++ messages.js
@@ -4,4 +4,9 @@ console.log("This is message.js");
 
 function boardMessage(res) {
     fs.readFile(__dirname + '/www/pages/board.html', 'utf-8')
+        .then(contents => {
+            res.end(contents);
+            console.log(contents);
+        });
 }
+boardMessage();
